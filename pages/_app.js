82cf9080d0630b84c40b0836c101d821f3721d35
Ken Trinh <ktrinh.particle@gmail.com>
2022-10-27 10:07:58 -0400

    apply auto ad

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
 import { ThemeProvider } from "next-themes";
+import Script from "next/script";
+
 import "../css/tailwind.css";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
+      <Script id="Adsense-id"
+        data-ad-client="ca-pub-9160996502338474"
+        async="true"
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
